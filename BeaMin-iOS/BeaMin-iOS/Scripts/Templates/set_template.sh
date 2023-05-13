247d39cee09102689e58f3c4930a493cc465befb
kimscastle <99013115+kimscastle@users.noreply.github.com>
2023-05-13 23:44:17 +0900

    [Setting]: 템플릿 구현

---
+++ BeaMin-iOS/BeaMin-iOS/Scripts/Templates/set_template.sh
@@ -0,0 +1,4 @@
+cp -r CollectionViewCell.xctemplate ~/Library/Developer/Xcode/Templates
+cp -r TableViewCell.xctemplate ~/Library/Developer/Xcode/Templates
+cp -r View.xctemplate ~/Library/Developer/Xcode/Templates
+cp -r ViewController.xctemplate ~/Library/Developer/Xcode/Templates
