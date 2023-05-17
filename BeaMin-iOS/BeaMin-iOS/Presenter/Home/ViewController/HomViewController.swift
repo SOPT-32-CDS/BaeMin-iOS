9d3074ecaec6f5650d35ead1adb448e5ce0809e2
ahra1221 <lizze011221@gmail.com>
2023-05-18 01:57:43 +0900

    [Feat] #4 CustomTabBar 화면에 붙이기

--- BeaMin-iOS/BeaMin-iOS/Presenter/Home/ViewController/HomViewController.swift
+++ BeaMin-iOS/BeaMin-iOS/Presenter/Home/ViewController/HomViewController.swift
@@ -5,4 +5,27 @@
 //  Created by uiskim on 2023/05/13.
 //
 
-import Foundation
+import UIKit
+
+class HomViewController: UIViewController {
+    let tabBarView = CustomTabBar(tabBarItems: [.find, .heart, .logo, .order, .mypage])
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tabBarView)
+        
+        tabBarView.translatesAutoresizingMaskIntoConstraints = false
+        tabBarView.layer.cornerRadius = 16
+        tabBarView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        NSLayoutConstraint.activate([
+            tabBarView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tabBarView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tabBarView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tabBarView.heightAnchor.constraint(equalToConstant: 83),
+            tabBarView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+        ])
+        
+    }
+}
