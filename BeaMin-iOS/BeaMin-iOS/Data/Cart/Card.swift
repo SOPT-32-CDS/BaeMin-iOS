76c2a14fc311f204399bc57693bf7ba86094e66a
kimscastle <99013115+kimscastle@users.noreply.github.com>
2023-05-18 19:20:37 +0900

    [Feat] #18 장바구니데이터구조추가 및 목데이터구성

---
+++ BeaMin-iOS/BeaMin-iOS/Data/Cart/Card.swift
@@ -0,0 +1,77 @@
+//
+//  Card.swift
+//  BeaMin-iOS
+//
+//  Created by uiskim on 2023/05/18.
+//
+
+import Foundation
+
+import DesignSystem
+import CustomExtension
+
+struct Cart {
+    
+    let menusByStroe: [MenuByStore]
+    
+    struct MenuByStore {
+        let storeImage: Constant.ImageName
+        let storeName: String
+        let cartMenus: [Menu]
+        let minimumPriceForDelivery: Int
+    }
+    
+    struct Menu {
+        let menuImage: Constant.ImageName
+        let menuName: String
+        let sideInfo: String?
+        let singleMenuPrice: Int
+        let totalPricePerMenu: Int
+        let menuCount: Int
+    }
+}
+
+extension Cart {
+    static var cartDummy: [Cart] {
+        return [
+            .init(menusByStroe: [
+                .init(storeImage: .logoImage,
+                      storeName: "청담초밥 송파점",
+                      cartMenus: sushiMenus,
+                      minimumPriceForDelivery: 2000),
+                .init(storeImage: .logoImage,
+                      storeName: "황후 탕후루",
+                      cartMenus: tanghuruMenus,
+                      minimumPriceForDelivery: 3000)
+            ])
+        ]
+    }
+    
+    static var sushiMenus: [Menu] {
+        return [
+            .init(menuImage: .menu17,
+                  menuName: "[갓성비]모둠초밥(10P)+미니우동",
+                  sideInfo: "새우튀김(6p)추가(7,000원)",
+                  singleMenuPrice: 11000,
+                  totalPricePerMenu: 36000,
+                  menuCount: 2),
+            .init(menuImage: .menu18,
+                  menuName: "[재주문1위]특초밥+미니우동",
+                  sideInfo: nil,
+                  singleMenuPrice: 12000,
+                  totalPricePerMenu: 16000,
+                  menuCount: 1)
+        ]
+    }
+    
+    static var tanghuruMenus: [Menu] {
+        return [
+            .init(menuImage: .menu10,
+                  menuName: "딸기탕후루 제철이 끝나가는중",
+                  sideInfo: nil,
+                  singleMenuPrice: 3800,
+                  totalPricePerMenu: 26600,
+                  menuCount: 7)
+        ]
+    }
+}
