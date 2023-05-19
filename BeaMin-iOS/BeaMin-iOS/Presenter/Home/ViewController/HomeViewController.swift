8a010340752091b1f40d8bfc78d84c6d0a65caad
kimscastle <99013115+kimscastle@users.noreply.github.com>
2023-05-19 19:29:00 +0900

    [Fix] #17 수정주석추가

--- BeaMin-iOS/BeaMin-iOS/Presenter/Home/ViewController/HomeViewController.swift
+++ BeaMin-iOS/BeaMin-iOS/Presenter/Home/ViewController/HomeViewController.swift
@@ -16,7 +16,8 @@ import DesignSystem
 
 final class HomeViewController: UIViewController {
 
-    private let promotionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    // MARK: - UICollectionViewFlowLayout입니다
+    private let promotionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     
     private let tabBarView = CustomTabBarView(tabBarItems: [.find, .heart, .logo, .order, .mypage])
     
@@ -31,20 +32,21 @@ final class HomeViewController: UIViewController {
         // MARK: - autolayout설정
         setLayout()
         
+        // MARK: - delegate설정
+        setDelegate()
     }
 }
 
 private extension HomeViewController {
     func setUI() {
         view.backgroundColor = .designSystem(.white)
-        HomePromotionCollectionViewCell.register(collectionView: promotionView)
         
-        promotionView.do {
-            $0.delegate = self
-            $0.dataSource = self
-        }
+        // MARK: - 이렇게 collectionView를 설정하는 부분은 setCollectionVIew같은 함수를 만들어서 viewdidload에서 호출하는게 좋을거같아요!
+        HomePromotionCollectionViewCell.register(collectionView: promotionView)
+
         
         tabBarView.do {
+            // MARK: - snapkit으로 바꾸면 translatesAutoresizingMaskIntoConstraints필요없습니다!
             $0.translatesAutoresizingMaskIntoConstraints = false
             $0.layer.cornerRadius = 16
             $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
@@ -63,6 +65,7 @@ private extension HomeViewController {
             $0.leading.trailing.bottom.equalToSuperview()
         }
         
+        // MARK: - 이부분 snapkit으로 바꿔주세요
         NSLayoutConstraint.activate([
             tabBarView.leftAnchor.constraint(equalTo: view.leftAnchor),
             tabBarView.rightAnchor.constraint(equalTo: view.rightAnchor),
@@ -71,20 +74,26 @@ private extension HomeViewController {
             tabBarView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
         ])
     }
+    
+    func setDelegate() {
+        promotionView.do {
+            $0.delegate = self
+            $0.dataSource = self
+        }
+    }
 }
 
+// MARK: - 저희 코드컨벤션중에 protocol을 나눠서 쓰자는 항목이 있었어서 UICollectionViewDelegate랑 UICollectionViewDataSource는 따로 쓰는게 좋아보입니다
 extension HomeViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // MARK: - 이부분도 제가 프로토콜 설명하면서 사용법 적어놨습니다!
+        // https://github.com/SOPT-32-CDS/BaeMin-iOS/pull/3 여기 5번 한번 확인해주세요!
+        let cell = HomePromotionCollectionViewCell.dequeueReusableCell(collectionView: collectionView, indexPath: indexPath)
+        return cell
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         print("🐰🐰🐰🐰🐰🐰")
         return 4
     }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        print("🍔🍔🍔🍔🍔")
-        guard let cell = promotionView.dequeueReusableCell(withReuseIdentifier: HomePromotionCollectionViewCell.identifier, for: indexPath) as? HomePromotionCollectionViewCell else { return UICollectionViewCell() }
-        print("🍔🍔🍔🍔🍔")
-        return cell
-        
-    }
-    
 }
