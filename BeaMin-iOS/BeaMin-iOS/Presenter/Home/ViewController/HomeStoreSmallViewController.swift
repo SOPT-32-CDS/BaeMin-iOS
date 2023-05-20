dc0f3d6437b5ddf8faf67edb4120ff69fe2336c8
ahra1221 <lizze011221@gmail.com>
2023-05-20 22:18:14 +0900

    [Feat] #17 HomeStoreSmall CollectionView 구현

--- BeaMin-iOS/BeaMin-iOS/Presenter/Home/ViewController/HomeStoreSmallViewController.swift
+++ BeaMin-iOS/BeaMin-iOS/Presenter/Home/ViewController/HomeStoreSmallViewController.swift
@@ -16,6 +16,10 @@ import DesignSystem
 
 final class HomeStoreSmallViewController: UIViewController {
     
+    private let homeStoreModel : [HomeStore] = HomeStore.dummyHomeStore()
+    
+    private lazy var homeStoreView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+    private let flowLayout = UICollectionViewFlowLayout()
 
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,14 +43,26 @@ final class HomeStoreSmallViewController: UIViewController {
 
 private extension HomeStoreSmallViewController {
     func setUI() {
+        view.backgroundColor = .designSystem(.white)
+        HomeStoreSmallCollectionViewCell.register(collectionView: homeStoreView)
         
+        flowLayout.do {
+            $0.scrollDirection = .horizontal
+            $0.itemSize = CGSize(width: 158, height: 283)
+        }
+
     }
     
     func setHierarchy() {
-        
+        view.addSubview(homeStoreView)
     }
     
     func setLayout() {
+        homeStoreView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(400)
+        }
         
     }
     
@@ -55,6 +71,24 @@ private extension HomeStoreSmallViewController {
     }
     
     func setDelegate() {
-        
+        homeStoreView.delegate = self
+        homeStoreView.dataSource = self
     }
+}
+
+extension HomeStoreSmallViewController : UICollectionViewDelegate {
+    
+}
+
+extension HomeStoreSmallViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = HomeStoreSmallCollectionViewCell.dequeueReusableCell(collectionView: homeStoreView, indexPath: indexPath)
+        cell.setDataBind(model: homeStoreModel[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
 }
