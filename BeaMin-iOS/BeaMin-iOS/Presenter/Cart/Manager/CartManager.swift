a483d4d17e2bc1dfc16c9549ad9714bedf40f53d
kimscastle <99013115+kimscastle@users.noreply.github.com>
2023-05-23 23:30:25 +0900

    [Feat] #21 매니저파일추가

---
+++ BeaMin-iOS/BeaMin-iOS/Presenter/Cart/Manager/CartManager.swift
@@ -0,0 +1,13 @@
+//
+//  CartManager.swift
+//  BeaMin-iOS
+//
+//  Created by uiskim on 2023/05/23.
+//
+
+import UIKit
+
+final class CartManager {
+    static let shared = CartManager()
+    private init() {}
+}
