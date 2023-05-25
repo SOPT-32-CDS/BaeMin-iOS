a0ac82ad2583841153ee9150065155cbc9396f57
kimscastle <99013115+kimscastle@users.noreply.github.com>
2023-05-25 13:03:57 +0900

    [Feat] #36 토큰, baseURL추가

--- BeaMin-iOS/CustomExtension/Constant/Constant.swift
+++ BeaMin-iOS/CustomExtension/Constant/Constant.swift
@@ -99,6 +99,11 @@ public struct Constant {
             return self.rawValue
         }
     }
+    
+    public struct CartNetworkConstant {
+        public static let baseURL: String = "http://43.201.68.44:8080/cart/"
+        public static let bearer: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWRkcmVzcyI6IuyGoe2MjOq1rCDsmKzrprztlL3roZwgMTM1In0.hh0main0EWtZYLHWlO3GfdrDgPDaAipNTkBlxgc5KSY"
+    }
 }
 
 extension UIImage {
