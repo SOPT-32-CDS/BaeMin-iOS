dd3729d0046cf8e5d0d933da2ef79a56c46b195d
kimscastle <99013115+kimscastle@users.noreply.github.com>
2023-05-25 13:01:32 +0900

    [Feat] #36 api모델추가

---
+++ BeaMin-iOS/BeaMin-iOS/Data/Cart/CartModel.swift
@@ -0,0 +1,55 @@
+//
+//  CartModel.swift
+//  BeaMin-iOS
+//
+//  Created by uiskim on 2023/05/25.
+//
+
+import Foundation
+
+struct CartModel: Codable {
+    let status: Int
+    let message: String
+    let data: DataClass
+    
+    struct DataClass: Codable {
+        let cartID, totalPrice, deliveryFee: Int
+        let cartStoreList: [CartStoreList]
+
+        enum CodingKeys: String, CodingKey {
+            case cartID = "cart_id"
+            case totalPrice = "total_price"
+            case deliveryFee = "delivery_fee"
+            case cartStoreList = "cart_store_list"
+        }
+    }
+
+    struct CartStoreList: Codable {
+        let cartStoreID: Int
+        let name: String
+        let image: String
+        let cartItemList: [CartItemList]
+
+        enum CodingKeys: String, CodingKey {
+            case cartStoreID = "cart_store_id"
+            case name, image
+            case cartItemList = "cart_item_list"
+        }
+    }
+
+    struct CartItemList: Codable {
+        let cartItemID: Int
+        let name: String
+        let totalPrice: Int
+        let options: String
+        let count: Int
+
+        enum CodingKeys: String, CodingKey {
+            case cartItemID = "cart_item_id"
+            case name
+            case totalPrice = "total_price"
+            case options, count
+        }
+    }
+
+}
